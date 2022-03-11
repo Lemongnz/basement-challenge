54b6fa34dcbd5ce0fb9556e4dcb41dedae49c29a
unknown <lemongnz@gmail.com>
2022-03-11 19:12:47 -0300

    day 1 ended

---
+++ pages/components/carousel-text/index.jsx
@@ -0,0 +1,12 @@
+import style from "./carousel-text.module.css";
+
+export default function CarouselText() {
+  return (
+    <div className={style.textWrapper}>
+      <span>
+        {" "}
+        A man can’t have enough base­ment swag  —  A man can’t have enough base­ment swag{" "}
+      </span>
+    </div>
+  );
+}
--- pages/components/header/index.tsx
+++ pages/components/header/index.tsx
@@ -2,9 +2,11 @@ import Image from "next/image";
 
 import footer from "../../../public/header.svg";
 
+import style from "./header.module.css";
+
 export default function Header() {
   return (
-    <div>
+    <div className={style.image}>
       <Image alt="header" src={footer} />
     </div>
   );
