b5a2e9a64a44947833c7c2972dad372487ba0afd
Matias Ringa <ringa.matias@gmail.com>
2022-03-11 21:20:26 +0100

    fix conflict

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import type {NextPage} from "next";
+import type { NextPage } from "next";
 
 import Nav from "./components/nav/nav";
 import Footer from "./components/footer";
@@ -7,15 +7,9 @@ import GridProducts from "./components/grid-products";
 
 const Home: NextPage = () => {
   return (
-<<<<<<< HEAD
     <div id="index" className="flex flex-col bg-black index">
       <header>
         <Nav></Nav>
-=======
-    <div className="flex flex-col bg-black index">
-      <header className="m-auto text-white text-center">
-        <Nav />
->>>>>>> 7727bbc6354bf61cdabed4f543c9c5915c24127e
       </header>
       <Header />
       <GridProducts />
