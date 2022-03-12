d6997e82ad10e523235e787d5f326b55a88e4679
unknown <lemongnz@gmail.com>
2022-03-12 11:50:39 -0300

    marquee added

--- pages/components/carousel-text/index.jsx
+++ pages/components/carousel-text/index.jsx
@@ -1,12 +1,16 @@
+import Marquee from "react-fast-marquee";
+
 import style from "./carousel-text.module.css";
 
 export default function CarouselText() {
   return (
     <div className={style.textWrapper}>
-      <span>
-        {" "}
-        A man can’t have enough base­ment swag  —  A man can’t have enough base­ment swag{" "}
-      </span>
+      <Marquee gradientColor={[0, 0, 0]} speed={70}>
+        <span>
+          {" "}
+          A man can’t have enough base­ment swag  —  A man can’t have enough base­ment swag{" "}
+        </span>
+      </Marquee>
     </div>
   );
 }
